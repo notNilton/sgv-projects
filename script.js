a715143091ece421032dcebc3f39168c33d0925a
notNilton <nilton.naab@gmail.com>
2023-10-19 13:31:29 -0400

    Update

--- script.js
+++ script.js
@@ -4,39 +4,35 @@ let selectedNode = null;
 let isDragging = false;
 let isDrawingConnector = false;
 let startConnector = null;
-let nodeCount = 0;
+let nodeConnection = [];
 
 function createNode(x, y) {
     const node = document.createElement('div');
     node.className = 'node';
-    // nodeCount++;
     node.id = 'node' + nodes.length;
     node.style.left = x + 'px';
     node.style.top = y + 'px';
     node.innerHTML = 'Node';
+    node.nodeConnection = null;
     document.getElementById('diagram-container').appendChild(node);
 
     // Adicione conectores à esquerda e à direita do nó
-    const leftConnector = createConnector(-5, '50%', node);
-    const rightConnector = createConnector('100%', '50%', node);
+    // const leftConnector = createConnector(-5, '50%', node);
+    // const rightConnector = createConnector('100%', '50%', node);
 
     node.addEventListener('mousedown', (e) => {
         selectedNode = node;
         isDragging = true;
     });
 
+    node.addEventListener('click', (e) =>{
+        if(isDragging == false){
+            console.log('test');
+        }
+    });
 
+    node.nodeConnection;
     return node;
-}
-
-
-function createConnector(left, top, node) {
-    const connector = document.createElement('div');
-    connector.className = 'connector';
-    connector.style.left = left;
-    connector.style.top = top;
-    node.appendChild(connector);
-    return connector;
 }
 
 document.getElementById('add-node').addEventListener('click', () => {
@@ -44,7 +40,10 @@ document.getElementById('add-node').addEventListener('click', () => {
     const y = Math.random() * 500;
     const node = createNode(x, y);
 
-    new LeaderLine(document.getElementById('element-1'), node, {color: 'red', size: 3});
+    // node.appendChild()
+    node.nodeConnection = new LeaderLine(document.getElementById('element-1'), node, {color: 'red', size: 3})
+    
+    console.log(node);
     nodes.push(node);
 });
 
@@ -52,7 +51,7 @@ document.addEventListener('mousemove', (e) => {
     if (isDragging) {
         selectedNode.style.left = e.clientX - selectedNode.offsetWidth / 2 + 'px';
         selectedNode.style.top = e.clientY - selectedNode.offsetHeight / 2 + 'px';
-        // path start and ending on svg
+        selectedNode.nodeConnection.position();
     }
 });
 
@@ -60,14 +59,16 @@ document.addEventListener('mouseup', () => {
     isDragging = false;
 });
 
-var startElement = document.getElementById('element-1'),
-  endElement = document.getElementById('element-2');
+createNode(200, 200);
+
+var startElement = document.getElementById('element-1');
+//   endElement = document.getElementById('element-2');
 
 // New leader line has red color and size 8.
 // new LeaderLine(startElement, endElement, {color: 'red', size: 8});
 
 
-// line1 = new LeaderLine(startElement, document.getElementById('node0'), {color: 'red', size: 3});
-// console.log(line1.end);
-// // createNode(200, 200);
+line1 = new LeaderLine(startElement, document.getElementById('node0'), {color: 'red', size: 3});
+console.log(line1.position());
+
 
