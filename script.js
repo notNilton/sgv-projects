d9c26f605f117a800c14ef2aaaf49c248ffd77fe
notNilton <nilton.naab@gmail.com>
2023-10-19 05:24:55 -0400

    Update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let nodes = [];
-let lines = [];
-let isDrawingLine = false;
-let startNode = null;
+let connectors = [];
+let selectedNode = null;
+let isDragging = false;
+let isDrawingConnector = false;
+let startConnector = null;
 
 function createNode(x, y) {
     const node = document.createElement('div');
@@ -11,19 +13,31 @@ function createNode(x, y) {
     node.innerHTML = 'Node';
     document.getElementById('diagram-container').appendChild(node);
 
-    const leftConnector = document.createElement('div');
-    leftConnector.className = 'connector';
-    leftConnector.style.left = '-5px';
-    leftConnector.style.top = '50%';
-    node.appendChild(leftConnector);
+    // Adicione conectores à esquerda e à direita do nó
+    const leftConnector = createConnector(-5, '50%', node);
+    const rightConnector = createConnector('100%', '50%', node);
 
-    const rightConnector = document.createElement('div');
-    rightConnector.className = 'connector';
-    rightConnector.style.right = '-5px';
-    rightConnector.style.top = '50%';
-    node.appendChild(rightConnector);
+    node.addEventListener('mousedown', (e) => {
+        selectedNode = node;
+        isDragging = true;
+    });
 
     return node;
+}
+
+function createConnector(left, top, node) {
+    const connector = document.createElement('div');
+    connector.className = 'connector';
+    connector.style.left = left;
+    connector.style.top = top;
+    node.appendChild(connector);
+    return connector;
+}
+
+function connectNodes(startNode, endNode) {
+    if (startNode !== endNode) {
+        drawConnector(startNode, endNode);
+    }
 }
 
 document.getElementById('add-node').addEventListener('click', () => {
@@ -32,35 +46,53 @@ document.getElementById('add-node').addEventListener('click', () => {
     const node = createNode(x, y);
     nodes.push(node);
 
-    if (isDrawingLine) {
-        isDrawingLine = false;
-        startNode = null;
+    if (selectedNode) {
+        if (isDrawingConnector) {
+            connectNodes(selectedNode, node);
+            isDrawingConnector = false;
+            startConnector = null;
+        }
     }
 });
 
 document.addEventListener('mousedown', (e) => {
-    if (e.target.classList.contains('connector')) {
-        if (!isDrawingLine) {
-            isDrawingLine = true;
-            startNode = e.target.parentNode;
+    if (e.target.classList.contains('left-connector') || e.target.classList.contains('right-connector')) {
+        if (!isDrawingConnector) {
+            isDrawingConnector = true;
+            startConnector = e.target;
         } else {
-            if (startNode && startNode !== e.target.parentNode) {
-                createLine(startNode, e.target.parentNode);
-                isDrawingLine = false;
-                startNode = null;
+            if (startConnector && startConnector !== e.target) {
+                connectNodes(startConnector.parentNode, e.target.parentNode);
+                isDrawingConnector = false;
+                startConnector = null;
             }
         }
     }
 });
 
-function createLine(startNode, endNode) {
-    const line = document.createElement('div');
-    line.className = 'line';
-    const startRect = startNode.getBoundingClientRect();
-    const endRect = endNode.getBoundingClientRect();
-    line.style.left = (startRect.left + startRect.width / 2) + 'px';
-    line.style.top = (startRect.top + startRect.height / 2) + 'px';
-    line.style.height = (endRect.top + endRect.height / 2 - startRect.top - startRect.height / 2) + 'px';
-    document.getElementById('diagram-container').appendChild(line);
-    lines.push(line);
-}
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        selectedNode.style.left = e.clientX - selectedNode.offsetWidth / 2 + 'px';
+        selectedNode.style.top = e.clientY - selectedNode.offsetHeight / 2 + 'px';
+        connectors.forEach(connector => {
+            document.getElementById('diagram-container').removeChild(connector);
+        });
+        connectors = [];
+        nodes.forEach(node => {
+            if (node !== selectedNode) {
+                const startRect = selectedNode.getBoundingClientRect();
+                const endRect = node.getBoundingClientRect();
+                if (startRect.right > endRect.left && startRect.left < endRect.right &&
+                    startRect.bottom < endRect.top && startRect.bottom + 10 > endRect.top) {
+                    connectNodes(selectedNode, node);
+                }
+            }
+        });
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+    isDrawingConnector = false;
+    startConnector = null;
+});
